5476b603bf2fae4d4689a5614b2df584dac4e129
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2025-11-09 23:35:26 -0500

    add rpmfusion and install intel-media-driver

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,13 +4,13 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# enable rpmfusion
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # this installs a package from fedora repos
 dnf5 install -y tmux 
+dnf install -y intel-media-driver
+dnf install -y nvtop
 
 # Use a COPR Example:
 #
@@ -20,5 +20,8 @@ dnf5 install -y tmux
 # dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
+
+# disable rpmfusion
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 systemctl enable podman.socket
