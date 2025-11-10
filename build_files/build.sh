44ff3ea50b415874e0a36aa1bd98a7c645ed1dc1
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2025-11-10 11:24:50 -0500

    remove rpmfusion removal line

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,8 +29,5 @@ dnf5 -y install helium
 # disable copr repos
 dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
 
-# disable rpmfusion
-dnf remove -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 #### Example for enabling a System Unit File
 systemctl enable podman.socket
