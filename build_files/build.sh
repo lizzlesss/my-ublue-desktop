7c0f1b0fd518acb3e01f526aa0346887fc52225c
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2025-11-10 17:53:54 -0500

    cleanup and add legcord

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,16 +6,21 @@ set -ouex pipefail
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # intel media driver and codecs
-dnf5 install -y intel-media-driver
-dnf5 install -y ffmpeg
-dnf5 install -y mpv
+dnf -y install \
+    intel-media-driver \
+    ffmpeg \
+    mpv
 
 # packages
-dnf5 install -y tmux 
-dnf5 install -y nvtop
+dnf -y install \
+    nvtop \
+    btop
 
 # nbfc-linux official rpm
 dnf5 install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.3.19/fedora-nbfc-linux-0.3.19-1.x86_64.rpm
+
+# legcord rpm (i just like legcord lol)
+dnf -y install https://github.com/Legcord/Legcord/releases/download/v1.1.6/Legcord-1.1.6-linux-x86_64.rpm
 
 # copr repos
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
