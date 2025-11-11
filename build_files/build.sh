f6b757fee7cd5612111ba5ec59d6347691f8c7f0
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2025-11-11 15:16:19 -0500

    add mangohud

--- build_files/build.sh
+++ build_files/build.sh
@@ -19,7 +19,8 @@ dnf -y group install \
 dnf -y install \
     nvtop \
     btop \
-    android-tools
+    android-tools \
+    mangohud
 
 # nbfc-linux official rpm
 dnf5 install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.3.19/fedora-nbfc-linux-0.3.19-1.x86_64.rpm
