a171cd332b87221dd26d67e7790b432f11175e99
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2025-11-14 00:51:06 -0500

    add adw-gtk3-theme

--- build_files/build.sh
+++ build_files/build.sh
@@ -23,8 +23,9 @@ dnf -y install \
     mangohud \
     steam \
     jmtpfs \
-    gvfs-mtp
-
+    gvfs-mtp \
+    adw-gtk3-theme
+    
 # nbfc-linux official rpm
 dnf5 install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.3.19/fedora-nbfc-linux-0.3.19-1.x86_64.rpm
 
