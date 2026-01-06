2d9561307d610d153f62c09100a9c6398700635b
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-01-06 14:01:29 -0500

    add firefox rpm (test)

--- build_files/build.sh
+++ build_files/build.sh
@@ -22,7 +22,8 @@ dnf -y install \
     steam \
     adw-gtk3-theme \
     btop \
-    waydroid
+    waydroid \
+    firefox
     
 # nbfc-linux official rpm
 dnf5 install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.3.19/fedora-nbfc-linux-0.3.19-1.x86_64.rpm
