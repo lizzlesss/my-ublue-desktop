56e21d9f45621d98368bb06e37568c4d71f1a805
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-01-12 19:51:00 -0500

    install cachyos kernel ???

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,6 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -ouex pipefail
+set -eoux pipefail
+
+# remove stock kernel
+for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
+  rpm --erase $pkg --nodeps
+done
+
+# disable initramfs generation by the kernel
+ln -sf /usr/bin/true /usr/lib/kernel/install.d/05-rpmostree.install
+ln -sf /usr/bin/true /usr/lib/kernel/install.d/50-dracut.install
+
+# install cachyos kernel
+dnf -y copr enable bieszczaders/kernel-cachyos
+dnf -y copr disable bieszczaders/kernel-cachyos
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos install \
+  kernel-cachyos \
+  kernel-cachyos-devel-matched
 
 # enable rpmfusion
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
