638f572790fc52f67de9d0d1c6a9b67e63d416f0
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-01-13 20:04:31 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -47,32 +47,3 @@ dnf -y copr disable mochaa/android-udev-rules
 systemctl enable podman.socket
 
 #### Example for enabling a System Unit File
-
-
-### kernel.sh ###
-
-# remove stock kernel
-for pkg in kernel kernel-core kernel-modules kernel-modules-core; do
-  rpm --erase $pkg --nodeps
-done
-
-# disable initramfs generation by the kernel
-ln -sf /usr/bin/true /usr/lib/kernel/install.d/05-rpmostree.install
-ln -sf /usr/bin/true /usr/lib/kernel/install.d/50-dracut.install
-
-# install cachyos kernel
-dnf -y copr enable bieszczaders/kernel-cachyos
-dnf -y copr disable bieszczaders/kernel-cachyos
-dnf -y --enablerepo copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos install \
-  kernel-cachyos \
-  kernel-cachyos-devel-matched
-
-
-### initramfs.sh ###
-
-KERNEL_VERSION="$(rpm -q --queryformat="%{evr}.%{arch}" kernel-core)"
-
-# Ensure Initramfs is generated
-export DRACUT_NO_XATTR=1
-/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
-chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
