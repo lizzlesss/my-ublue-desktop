d82a297f330b9415aab85e0b8e4b0e4f1a32c268
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-01-17 16:20:42 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,15 +2,15 @@
 
 set -ouex pipefail
 
-# edition="$1"
+edition="$1"
 
 
-# if [[ "$edition" == "workstation" ]]; then
-/ctx/packages/kernel.sh \
-/ctx/packages/pkgs.sh 
+if [[ "$edition" == "workstation" ]]; then
+    /ctx/packages/kernel.sh \
+    /ctx/packages/pkgs.sh 
 
-#else
-    #echo "Invalid edition specified. Please use 'workstation' or 'multimedia'."
-    #exit 1
-#fi
+else
+    echo "Invalid edition specified. Please use 'workstation' or 'multimedia'."
+    exit 1
+fi
 
