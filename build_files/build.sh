757c932815644aa28ad4ea219af8922c4d223fd9
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-01-17 16:03:03 -0500

    Create build.sh

---
+++ build_files/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# edition="$1"
+
+
+# if [[ "$edition" == "workstation" ]]; then
+/ctx/packages/kernel.sh \
+/ctx/packages/pkgs.sh 
+
+#else
+    #echo "Invalid edition specified. Please use 'workstation' or 'multimedia'."
+    #exit 1
+#fi
+
