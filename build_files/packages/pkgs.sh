6ccef61b9fdec6e1a5ab086dc4eb68af48c92fd8
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-03-17 23:05:15 -0400

    Update pkgs.sh

--- build_files/packages/pkgs.sh
+++ build_files/packages/pkgs.sh
@@ -3,7 +3,7 @@
 set -eoux pipefail
 
 # enable rpmfusion
-dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # intel media driver and codecs
 dnf -y install \
@@ -24,7 +24,7 @@ dnf -y install \
     chromium
     
 # nbfc-linux official rpm
-dnf5 install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.3.19/fedora-nbfc-linux-0.3.19-1.x86_64.rpm
+dnf install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.3.19/fedora-nbfc-linux-0.3.19-1.x86_64.rpm
 
 dnf -y copr enable mochaa/android-udev-rules
 
