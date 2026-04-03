9ae9a5a44d0d3c3ba2bbc45c376e4d0bbfda4732
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-04-02 21:37:01 -0400

    Update pkgs.sh

--- build_files/packages/pkgs.sh
+++ build_files/packages/pkgs.sh
@@ -23,17 +23,17 @@ dnf -y install \
 # nbfc-linux official rpm
 dnf install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.3.19/fedora-nbfc-linux-0.3.19-1.x86_64.rpm
 
-#dnf -y copr enable mochaa/android-udev-rules
-dnf -y copr enable crono/system76-scheduler 
+dnf -y copr enable mochaa/android-udev-rules fedora-rawhide-x86_64
+dnf -y copr enable crono/system76-scheduler fedora-rawhide-x86_64
 
 
 # copr packages
 dnf -y install \
-    #android-udev-rules \
+    android-udev-rules \
     system76-scheduler-git
     
-#dnf -y copr disable mochaa/android-udev-rules
-dnf -y copr disable kylegospo/system76-scheduler
+dnf -y copr disable mochaa/android-udev-rules fedora-rawhide-x86_64
+dnf -y copr disable kylegospo/system76-scheduler fedora-rawhide-x86_64
 
 systemctl enable podman.socket
 
