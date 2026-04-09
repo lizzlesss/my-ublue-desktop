ed71c9180ec338161739083aa9abeb36a4895082
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-04-09 16:08:31 -0400

    update nbfc

--- build_files/packages/pkgs.sh
+++ build_files/packages/pkgs.sh
@@ -21,7 +21,7 @@ dnf -y install \
     waydroid
     
 # nbfc-linux official rpm
-dnf install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.3.19/fedora-nbfc-linux-0.3.19-1.x86_64.rpm
+dnf install -y https://github.com/nbfc-linux/nbfc-linux/releases/download/0.4.0/fedora-nbfc-linux-0.4.0-1.x86_64.rpm
 
 dnf -y copr enable mochaa/android-udev-rules fedora-rawhide-x86_64
 dnf -y copr enable crono/system76-scheduler fedora-rawhide-x86_64
